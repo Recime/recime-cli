e3522adf8f50656e59165b9abe91639754b379d9
Mehfuz Hossain <mehfuz@gmail.com>
2016-09-16 01:04:25 -0700

    fixes

--- deploy.go
+++ deploy.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 import "os"
 
-// import "bufio"
+import "bufio"
 
 import "bytes"
 
@@ -16,8 +16,38 @@ import "encoding/json"
 import "os/exec"
 
 import "net/http"
-import "mime/multipart"
-import fs "path/filepath"
+
+// import "mime/multipart"
+// import fs "path/filepath"
+
+// import "gopkg.in/cheggaaa/pb.v1"
+import "github.com/briandowns/spinner"
+import "time"
+
+type Bot struct{
+    Id string `json:"uid"`
+    Type string `json:"fileType"`
+}
+
+func SendRequest(url string, body io.Reader) (string){
+    res, err := http.Post(url, "application/json; charset=utf-8", body)
+
+    check(err)
+
+    var result struct {
+        Url string `json:"url"`
+    }
+
+    bytes, err := ioutil.ReadAll(res.Body)
+
+    json.Unmarshal(bytes, &result)
+
+    defer res.Body.Close()
+
+    // fmt.Println(string(res.Body))
+
+    return result.Url
+}
 
 
 func Deploy() {
@@ -38,7 +68,7 @@ func Deploy() {
 
   uid := data["uid"].(string)
 
-  fmt.Println("Installing modules")
+  fmt.Println("INFO: Installing modules.")
 
   cmd := exec.Command("npm", "install")
 
@@ -51,7 +81,7 @@ func Deploy() {
 
   check(err)
 
-  fmt.Println("Compressing")
+  fmt.Println("INFO: Compressing.")
 
   temp, err :=  ioutil.TempDir("", name)
 
@@ -73,51 +103,119 @@ func Deploy() {
 
   defer file.Close()
 
-  body := &bytes.Buffer{}
+  // body := &bytes.Buffer{}
+  //
+  // writer := multipart.NewWriter(body)
+  //
+  // params := map[string]string {
+  //     "uid" : uid,
+  // }
+  //
+  // part, err := writer.CreateFormFile("file", fs.Base(filePath))
+  //
+  // check(err)
+  //
+  // _, err = io.Copy(part, file)
+  //
+  // for key, val := range params {
+  //     _ = writer.WriteField(key, val)
+  // }
+  // err = writer.Close()
+  //
+  // check(err)
 
-  writer := multipart.NewWriter(body)
+  //read file content to buffer
 
-  params := map[string]string {
-      "uid" : uid,
-  }
+  fileInfo, _ := file.Stat()
 
-  part, err := writer.CreateFormFile("file", fs.Base(filePath))
+  var size = fileInfo.Size()
+
+  buffer := make([]byte, size)
+
+  // read file content to buffer
+  file.Read(buffer)
+
+  fmt.Println("INFO: Preparing to upload.")
+
+  url := "http://recimedev-env.us-west-1.elasticbeanstalk.com/signed-url"
+
+  fileType := http.DetectContentType(buffer)
+
+  bot := Bot { Id : uid, Type : fileType, }
+
+  jsonBody, err := json.Marshal(bot)
 
   check(err)
 
-  _, err = io.Copy(part, file)
+  signedUrl := SendRequest(url, bytes.NewBuffer(jsonBody))
 
-  for key, val := range params {
-      _ = writer.WriteField(key, val)
-  }
-  err = writer.Close()
+  // fmt.Println(signedUrl)
+
+  // url = "http://localhost:8081/module/deploy/" + name
+
+  fileReader := bytes.NewReader(buffer)
+
+  // bar := pb.New(int(size)).SetUnits(pb.U_BYTES)
+  //
+  // bar.Start()
+
+  // _ = bar.NewProxyReader(fileReader)
+
+
+  // proxy := &ioprogress.Reader{
+  //   Reader: fileReader,
+  //   Size:   size,
+  // }
+
+  req, err := http.NewRequest("PUT", signedUrl, fileReader)
+
+  s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)  // Build our new spinner
+
+  s.Start()
 
   check(err)
 
-  fmt.Println("Preparing to Upload")
-
-  req, err := http.NewRequest("POST", "http://localhost:8081/module/deploy/" + name, body)
-
-  req.Header.Set("Content-Type", writer.FormDataContentType())
-
-  check(err)
+  // bar.Finish()
 
   resp, err := http.DefaultClient.Do(req)
 
   check(err)
 
+  defer resp.Body.Close()
+
   dat, err := ioutil.ReadAll(resp.Body)
 
-  // for {
-  //     line, err := reader.ReadBytes('\n')
-  //     line = bytes.TrimSpace(line)
-  //
-  //     if err != nil {
-  //       break
-  //     }
-  //
-      fmt.Println(string(dat))
-  // }
+  s.Stop()
+
+  if len(dat) == 0 {
+    fmt.Println("INFO: Finalizing.")
+  }
+
+
+  //bar.Finish()
+
+  url = "http://recimedev-env.us-west-1.elasticbeanstalk.com/module/deploy/" + name
+
+  s.Start()
+
+  resp, err = http.Post(url, "application/json; charset=utf-8", bytes.NewBuffer(jsonBody))
+
+  check(err)
+
+  reader := bufio.NewReader(resp.Body)
+
+  for {
+      line, err := reader.ReadBytes('\n')
+      line = bytes.TrimSpace(line)
+
+      fmt.Println(string(line))
+
+      if err == io.EOF {
+        s.Stop()
+        break
+      }
+  }
 
   defer resp.Body.Close()
+
 }
