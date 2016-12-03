312689c1851ae0fe5717b022fed3bf734131d4e5
Mehfuz Hossain <mehfuz@gmail.com>
2016-12-02 19:50:44 -0800

    fixes

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package main
 
 // BaseURL Recime cloud
-const BaseURL = "http://recime.ai"
+const BaseURL = "https://recime.ai"
 
 // AppTemplateURL node template
 const AppTemplateURL = "https://github.com/Recime/recime-template/archive/1.0.1.zip"
