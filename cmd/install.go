a60722fd165d0c39126a380bfe59035cbdfb8074
Mehfuz Hossain <mehfuz@gmail.com>
2016-12-02 18:12:21 -0800

    fixes

--- cmd/install.go
+++ cmd/install.go
@@ -4,18 +4,28 @@ import "fmt"
 import "os"
 import "os/exec"
 
+// PrintError prints error message
+func PrintError(err error) {
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("\n\r> Error: %s\n", err.Error()))
+	}
+}
+
+// Install installs dependencies.
 func Install() {
-	fmt.Println("INFO: Installing dependencies.")
+	fmt.Println("INFO: Installing Dependencies.")
 
 	wd, err := os.Getwd()
 
 	check(err)
 
-	cmd := exec.Command("npm", "install")
+	cmd := exec.Command("npm1", "install")
 
 	cmd.Dir = wd
 
 	cmd.Stdout = os.Stdout
 
-	cmd.Run()
+	err = cmd.Run()
+
+	PrintError(err)
 }
--- constants.go
+++ constants.go
@@ -6,5 +6,5 @@ const BaseURL = "https://recime.ai"
 // AppTemplateURL node template
 const AppTemplateURL = "https://github.com/Recime/recime-template/archive/1.0.1.zip"
 
-// Version 1.0.7
-const Version = "1.0.7"
+// Version 1.0.8
+const Version = "1.0.8"
