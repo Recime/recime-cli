477edf017c9ac50ddacfeb6b3fa70dc03eb801d0
Mehfuz Hossain <mehfuz@gmail.com>
2016-12-05 10:56:19 -0800

    Script Engine fixes fixes #5

--- constants.go
+++ constants.go
@@ -4,7 +4,7 @@ package main
 const BaseURL = "https://recime.ai"
 
 // AppTemplateURL node template
-const AppTemplateURL = "https://github.com/Recime/recime-template/archive/1.0.1.zip"
+const AppTemplateURL = "https://github.com/Recime/recime-template/archive/1.0.2.zip"
 
 // Version 1.0.8
-const Version = "1.0.8"
+const Version = "1.0.9"
