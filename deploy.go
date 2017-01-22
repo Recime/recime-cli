150852e936e0556cb81eb275e9372f4f22cb0a33
Mehfuz Hossain <mehfuz@gmail.com>
2017-01-22 00:04:23 -0800

    refactor

--- deploy.go
+++ deploy.go
@@ -11,8 +11,6 @@ import (
 	"time"
 
 	"path/filepath"
-	"archive/zip"
-	"strings"
 
 	"github.com/Recime/recime-cli/cmd"
 	"github.com/Recime/recime-cli/util"
@@ -79,83 +77,9 @@ func PrepareLambdaPackage(uid string) string {
 
 	pkg := filepath.ToSlash(fmt.Sprintf("%s/%s.zip", temp, uid))
 
-	Zip(target, pkg)
-
-	// zip := new(archivex.ZipFile)
-	// zip.Create(pkg)
-	// zip.AddAll(target, true)
-	// zip.Close()
+	util.Zip(target, pkg)
 
 	return pkg
-}
-
-func Zip(source, target string) error {
-	zipfile, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-	defer zipfile.Close()
-
-	archive := zip.NewWriter(zipfile)
-	defer archive.Close()
-
-	info, err := os.Stat(source)
-	if err != nil {
-		return nil
-	}
-
-	var baseDir string
-
-	if info.IsDir() {
-		baseDir = filepath.Base(source)
-	}
-
-	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-
-		if err != nil {
-			return err
-		}
-
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-
-		if baseDir != "" {
-			path = filepath.ToSlash(path)
-			source = filepath.ToSlash(source)
-
-			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
-		}
-
-		if info.IsDir() {
-			header.Name += "/"
-		} else {
-			header.Method = zip.Deflate
-		}
-
-		header.Name = filepath.ToSlash(header.Name)
-
-		writer, err := archive.CreateHeader(header)
-
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			return nil
-		}
-
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(writer, file)
-		return err
-	})
-
-	return err
 }
 
 // SendRequest sends POST request
