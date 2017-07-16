363fb0d90fce4bbf227886ed73eb6dda7b240d63
Mehfuz Hossain <mehfuz@gmail.com>
2017-07-16 00:54:42 -0700

    updates

--- constants.go
+++ constants.go
@@ -16,10 +16,10 @@ package main
 // SiteURL Site URL
 const SiteURL = "https://recime.io"
 
-// Version 1.3.3
-const Version = "1.3.3"
+// Version 1.3.4
+const Version = "1.3.4"
 
 const apiEndpoint = "https://api.recime.io/v1"
-const template = "https://github.com/Recime/recime-bot-template/archive/1.0.8.zip"
+const template = "https://github.com/Recime/recime-bot-template/archive/1.0.9.zip"
 const es6BotTemplate = "https://github.com/Recime/recime-bot-es6-template/archive/1.0.2.zip"
 const typescriptBotTemplate = "https://github.com/Recime/recime-bot-typescript-template/archive/1.0.3.zip"
